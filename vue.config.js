aa80d41b8237403ee9c2402a5e1a8a50e615f7db
lt <18603457721@163.com>
2020-10-20 18:38:35 +0800

    commit

--- vue.config.js
+++ vue.config.js
@@ -57,20 +57,12 @@ module.exports = {
     devServer: {
         port: 3000,
         proxy: {
-            /* '/api': {
-        target: 'https://mock.ihx.me/mock/5baf3052f7da7e07e04a5116/antd-pro', //mock API接口系统
-        ws: false,
-        changeOrigin: true,
-        pathRewrite: {
-          '/jeecg-boot': ''  //默认所有请求都加了jeecg-boot前缀，需要去掉
-        }
-      }, */
-            '/jeecg-boot': {
-                target: 'http://49.232.106.88:8082', // 请求本地 需要jeecg-boot后台项目
-                //target: 'http://10.21.0.57:8082',
-                ws: false,
-                changeOrigin: true
-            }
+          '/jeecg-boot': {
+              target: 'http://49.232.106.88:8082', // 请求本地 需要jeecg-boot后台项目
+              //target: 'http://10.21.0.57:8082',
+              ws: false,
+              changeOrigin: true
+          }
         }
     },
 
